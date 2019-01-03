f44e09ab6a51c5aac72e71deb0cbdaac68020d8d
Diptiman Chakrabarti <diptiman.chakrabarti@in.ibm.com>
2019-01-02 20:30:52 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
+let db, trips, expenses,memberinfo
+mongo.connect(
+    url,
+    (err, client) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      db = client.db('tripcost')
+      trips = db.collection('trips')
+      expenses = db.collection('expenses')
+      memberinfo=db.collection('memberinfo')
+    }
+  )
+
+const app = express()
+app.use(express.json())
+app.post('/trip', (req, res) => {
+    const name = req.body.name
+    trips.insertOne({ name: name }, (err, result) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+      }
+      console.log(result)
+      res.status(200).json({ ok: true })
+    })
+  })
+  app.get('/trips', (req, res) => {
+	trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ trips: items })
+	})
+})
+app.post('/expense', (req, res) => {
+    expenses.insertOne(
+      {
+        trip: req.body.trip,
+        date: req.body.date,
+        amount: req.body.amount,
+        category: req.body.category,
+        description: req.body.description
+      },
+      (err, result) => {
+        if (err) {
+          console.error(err)
+          res.status(500).json({ err: err })
+          return
+        }
+        res.status(200).json({ ok: true })
+      }
+    )
+  })
+  app.get('/expenses', (req, res) => {
+    expenses.find({trip: req.body.trip}).toArray((err, items) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+      }
+      res.status(200).json({ trips: items })
+    })
+  })
+  app.post('/members', (req, res) => {
+    var id=req.body.members.member
+    var arr1=[]
+    const alnth=id.length
+    for(var i=0; i<alnth; i++) {
+      arr1.push(id[i]["id"])
+    }
+    console.log(arr1)
+    const mbrid1=id[0]["id"]
+    const mbrid2=id[1]["id"]
+    var arr=[mbrid1,mbrid2]
+    memberinfo.find({memberid:{ $in:arr1}}).toArray((err, items) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+      }
+      res.status(200).json({ members: items })
+    })
+  })
+app.listen(3000, () => console.log('Server ready'))
